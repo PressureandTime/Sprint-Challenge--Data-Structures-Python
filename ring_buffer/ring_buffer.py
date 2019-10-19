f128ec294ef96036dcec57c35025fb54b30d6b30
Petar Scepanovic <hiborija@gmail.com>
2019-10-19 10:49:33 +0200

    continuing with sprint, added more code in append method, testing it now

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,23 +1,21 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None] * capacity
 
-  def append(self, item):
-    i = self.current
+    def append(self, item):
+        i = self.current
+        self.storage[i] = item
+        self.storage += 1
 
-    self.storage[i] = item
-
-
-  def get(self):
-    pass
-
+    def get(self):
+        pass
 
 
 buffer = RingBuffer(3)
-buffer.append('petar')
-buffer.append('shaun')
-buffer.append('luke')
+buffer.append("petar")
+buffer.append("shaun")
+buffer.append("luke")
 
 print(buffer.storage)
